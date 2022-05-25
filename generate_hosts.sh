ac7686c0d4fdbb21bf35107f02c9566e7058eeba
kosoewin <53328736+kosoewin@users.noreply.github.com>
2022-05-25 14:04:48 +0630

    Update generate_hosts.sh

--- generate_hosts.sh
+++ generate_hosts.sh
@@ -4,5 +4,5 @@
 sudo bash -c 'cat >> /etc/hosts' << EOF
 192.168.101.10	ansiblecontrol.ayalab.local  ansiblecontrol
 192.168.101.20	ansiblenode01.ayalab.local  ansiblenode01
-192.168.101.20	ansiblenode02.ayalab.local  ansiblenode02
+192.168.101.30	ansiblenode02.ayalab.local  ansiblenode02
 EOF
