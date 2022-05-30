392a1606eaff4c35b2b2c249d3b822ace1270a30
kosoewin <53328736+kosoewin@users.noreply.github.com>
2022-05-30 13:56:00 +0630

    Update controlnode.sh

--- controlnode.sh
+++ controlnode.sh
@@ -1,5 +1,5 @@
 # This script will be created prerequisites for controlnode
-
+       sudo yum install epel-release -y
        sudo yum install -y ansible python3
        ansible --versión
        sudo alternatives --set python /usr/bin/python3
@@ -7,6 +7,4 @@
        sudo echo " password" | passwd --stdin ansible
        sudo echo " ansible ALL=(ALL) NOPASSWD:ALL " > /etc/sudoers.d/ansible
        su ansible; ssh-keygen
-       ssh-copy-id node02
-       ssh-copy-id node01
-       ssh-copy-id ansiblecontrol
+       
